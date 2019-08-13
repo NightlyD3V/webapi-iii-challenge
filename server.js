99756c032c398c0ceb2502cbeb359647d82201c3
Tyler Spaulding <TylerSpaulding95@gmail.com>
2019-08-13 17:51:57 -0400

    Inital commit and project set-up

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = 'express';
-
+const express = require('express');
+const helmet = require('helmet');
 const server = express();
+
+server.use(helmet('development'));
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
+});
+
+server.listen(4000, () => {
+  console.log('server listening on port 4000');
 });
 
 //custom middleware
