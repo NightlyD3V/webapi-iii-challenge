a9ad04d14b5377a6ff19ec760aa6488a09528b4e
Tyler Spaulding <TylerSpaulding95@gmail.com>
2019-08-13 19:22:06 -0400

    Setting up middleware and user router

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const helmet = require('helmet');
 const server = express();
 
-server.use(helmet('development'));
+const User = require('./users/userDb.js');
+
+server.use(helmet());
 server.use(express.json());
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -16,7 +19,50 @@ server.listen(4000, () => {
 //custom middleware
 
 function logger(req, res, next) {
+  console.log(`req.method: ${req.method}, req.url: ${req.url}, timestamp: ${Date.now()}`);
+  next();
+};
 
+function validateUserId(req, res, next) {
+    const { id } = req.params;
+
+    User.getById(id)
+    .then(user => {
+      if(user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(500).json({
+          message: 'Invalid user ID'
+        });
+      };
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: 'Error processing request!'
+      })
+    })
+};
+
+function validateUser(req, res, next) {
+
+};
+
+function validatePost(req, res, next) {
+  const body = req.body;
+
+  if(body && Object.keys(req.body).length > 0) {
+    next();
+  } else if(req.body.text === '') {
+    res.status(400).json({
+      message: 'Missing required text field'
+    })
+  }else {
+    res.status(500).json({
+      message: "Missing post data!"
+    })
+  }
 };
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,17 +1,35 @@
 const express = 'express';
-
+const User = require('./users/userDb.js');
+const Post = require('../posts/postDb.js');
 const router = express.Router();
 
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+    
 });
 
-router.post('/:id/posts', (req, res) => {
-
+router.post('/:id/posts', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const post = await Post.getById(id);
+        res.status(200).json(post.body)
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({
+            message: `Error getting post with that ID: ${id}`
+        })
+    }
 });
 
 router.get('/', (req, res) => {
-
+    try {
+        const users = await User.get();
+        res.status(200).json(users.body)
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Error getting users!'
+        })
+    }
 });
 
 router.get('/:id', (req, res) => {
